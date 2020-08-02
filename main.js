ed60c4e63ba4aab80f63488532559bd7d2b3a48c
Stuart Wong <swong31@gmail.com>
2020-08-02 16:28:03 -0700

    Added data-values to row sections

--- main.js
+++ main.js
@@ -1,45 +1,54 @@
 // Moment JS to show day
-var now = moment().format("dddd, MMMM Do")
-var currentDay = $("#currentDay")
-currentDay.append(now)
+var now = moment().format("dddd, MMMM Do");
+var currentDay = $("#currentDay");
+currentDay.append(now);
 
-// Div to append to 
+// Div to append to
 var schedule = $("#schedule");
 // Array for hours of the day
 var timeSlot = [
-  "9am",
-  "10am",
-  "11am",
-  "12pm",
-  "1pm",
-  "2pm",
-  "3pm",
-  "4pm",
-  "5pm",
+  "9AM",
+  "10AM",
+  "11AM",
+  "12PM",
+  "1PM",
+  "2PM",
+  "3PM",
+  "4PM",
+  "5PM",
 ];
 
-function rowBuilder() {
-  // Build the schedule by row
-  for (var i = 0; i < timeSlot.length; i++) {
-    var newRow = $("<div>");
-    newRow.addClass("row hour");
-    // newRow.data("hour", hour); bug here; need to figure out how to add data value
-    // Time section of row
-    var timeBlock = $("<div>");
-    timeBlock.addClass("col-sm-2 time-block");
-    timeBlock.text(timeSlot[i]);
-    // Input Section of row
-    var input = $("<span>");
-    input.addClass("col-lg-9");
-    // Button section of row
-    var btn = $("<button>");
-    btn.addClass("col-sm-1 saveBtn");
-    // Add sections to row
-    newRow.append(timeBlock);
-    newRow.append(input);
-    newRow.append(btn);
-    // Add row to schedule
-    schedule.append(newRow);
+// Build the schedule by row
+for (var i = 0; i < timeSlot.length; i++) {
+  var newRow = $("<div>");
+  newRow.addClass("row");
+  // Time section of row
+  var timeBlock = $("<div>");
+  timeBlock.addClass("col-sm-2 time-block");
+  timeBlock.text(timeSlot[i]);
+  // Input Section of row
+  var input = $("<input>");
+  input.addClass("col-lg-9 hour");
+  input.attr("data-value", timeSlot[i])
+  // Button section of row
+  var btn = $("<button>");
+  btn.addClass("col-sm-1 saveBtn");
+  btn.text("Save");
+  // Add sections to row
+  newRow.append(timeBlock);
+  newRow.append(input);
+  newRow.append(btn);
+  // Add row to schedule
+  schedule.append(newRow);
+}
+
+
+function rowColor() {
+  var hourCurrent = moment().format("hA");
+  var current = input.attr("data-value")
+  console.log(current)
+  if (hourCurrent === current) {
+    input.addClass("present");
   }
 }
-rowBuilder();
+rowColor()
