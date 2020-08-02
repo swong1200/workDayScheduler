dd8421226c1da5b0b44cf86bca50e4e34fb850ca
Stuart Wong <swong31@gmail.com>
2020-08-01 21:07:02 -0700

    Changed main.js plus added current day using Moment JS

--- main.js
+++ main.js
@@ -1,8 +1,45 @@
-var nine = $("#nine")
+// Moment JS to show day
+var now = moment().format("dddd, MMMM Do")
+var currentDay = $("#currentDay")
+currentDay.append(now)
 
-function appointment () {
-    var p = $("<p>")
-    p.text("hello world")
-    nine.append(p)
+// Div to append to 
+var schedule = $("#schedule");
+// Array for hours of the day
+var timeSlot = [
+  "9am",
+  "10am",
+  "11am",
+  "12pm",
+  "1pm",
+  "2pm",
+  "3pm",
+  "4pm",
+  "5pm",
+];
+
+function rowBuilder() {
+  // Build the schedule by row
+  for (var i = 0; i < timeSlot.length; i++) {
+    var newRow = $("<div>");
+    newRow.addClass("row hour");
+    // newRow.data("hour", hour); bug here; need to figure out how to add data value
+    // Time section of row
+    var timeBlock = $("<div>");
+    timeBlock.addClass("col-sm-2 time-block");
+    timeBlock.text(timeSlot[i]);
+    // Input Section of row
+    var input = $("<span>");
+    input.addClass("col-lg-9");
+    // Button section of row
+    var btn = $("<button>");
+    btn.addClass("col-sm-1 saveBtn");
+    // Add sections to row
+    newRow.append(timeBlock);
+    newRow.append(input);
+    newRow.append(btn);
+    // Add row to schedule
+    schedule.append(newRow);
+  }
 }
-appointment()
+rowBuilder();
